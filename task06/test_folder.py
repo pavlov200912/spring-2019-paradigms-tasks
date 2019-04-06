9b5d72670681445afc54fcd1c3d7f9bf77cd18d3
pavlov200912 <pavlov200912@gmail.com>
2019-04-07 00:42:15 +0300

    tests implemented

---
+++ task06/test_folder.py
@@ -0,0 +1,54 @@
+import pytest
+from folder import *
+
+def test_number_and_number():
+    assert fold_constants(BinaryOperation(
+        Number(21), '+', Number(10))) == Number(31)
+    assert fold_constants(BinaryOperation(
+        Number(7), '*', Number(8))) == Number(56)
+    assert fold_constants(BinaryOperation(
+        Number(22), '==', Number(3))) == Number(0)
+    assert fold_constants(BinaryOperation(
+        Number(222), '/', Number(2))) == Number(111)
+
+
+ def test_zero_multiply_reference():
+    assert fold_constants(BinaryOperation(
+        Number(0), '*', Reference('x'))) == Number(0)
+
+
+ def test_reference_multiply_zero():
+    assert fold_constants(BinaryOperation(
+        Reference('x'), '*', Number(0))) == Number(0)
+
+
+ def test_reference_substract_reference():
+    assert fold_constants(BinaryOperation(
+        Reference('x'), '-', Reference('x'))) == Number(0)
+
+
+ def test_unary_operation_and_number():
+    assert fold_constants(UnaryOperation('!', Number(0))) == Number(1)
+    assert fold_constants(UnaryOperation('!', Number(1))) == Number(0)
+    assert fold_constants(UnaryOperation('-', Number(7))) == Number(-7)
+
+
+ def test_end_to_end():
+    assert fold_constants(
+        BinaryOperation(
+            Number(10),
+            '-',
+            UnaryOperation(
+                '-',
+                BinaryOperation(
+                    Number(3),
+                    '+',
+                    BinaryOperation(
+                        Reference('x'),
+                        '-',
+                        Reference('x')
+                    )
+                )
+            )
+        )
+    ) == Number(13)
